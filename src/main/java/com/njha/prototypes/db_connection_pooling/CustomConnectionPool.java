4130f400f1e2093d026fc4c405db6438aef67111
Narendra Jha <njha.sde@gmail.com>
2024-08-25 09:06:01 +0530

    don't use Spring Hikari pool connection

--- src/main/java/com/njha/prototypes/db_connection_pooling/CustomConnectionPool.java
+++ src/main/java/com/njha/prototypes/db_connection_pooling/CustomConnectionPool.java
@@ -2,12 +2,10 @@ package com.njha.prototypes.db_connection_pooling;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -19,14 +17,16 @@ public class CustomConnectionPool {
 
     private BlockingQueue<Connection> connectionPool;
 
-    @Autowired
-    private DataSource dataSource;
+    private String url = "jdbc:postgresql://localhost:5432/test";
+    private String user = "narendrajha";
+    private String password = "narendrajha";
 
     @PostConstruct
     public void initializeConnectionPool() throws SQLException {
         connectionPool = new ArrayBlockingQueue<>(POOL_SIZE);
         for (int i = 0; i < POOL_SIZE; i++) {
-            connectionPool.add(dataSource.getConnection());
+            Connection connection = DriverManager.getConnection(url, user, password);
+            connectionPool.add(connection);
         }
     }
 
